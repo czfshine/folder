2bbf50928ace7e56430b0cd3c69808bf3ae4d8f1
czfshine <czfshine@outlook.com>
2018-04-15 11:26:29 +0800

    修复环境路径

--- fl.lua
+++ fl.lua
@@ -1,10 +1,20 @@
 #!/usr/bin/lua
 --conding:utf-8
 appdata=os.getenv("APPDATA") or ""
-package.path=package.path..";;"..appdata..
-    "\\folder\\luagy\\Penlight\\lua\\?.lua;"..
-    appdata.."\\folder\\luagy\\?.lua;"..appdata..
-    "\\folder\\?.lua;/usr/share/folder/?.lua;/usr/share/folder/luagy/?.lua;/usr/share/folder/luagy/Penlight/lua/?.lua;".."./luagy/?.lua;./luagy/Penlight/lua/?.lua"
+function addpath(path)
+    package.path=package.path..";"..path
+end
+
+addpath(appdata.."\\folder\\luagy\\Penlight\\lua\\?.lua")
+addpath("/usr/share/folder/luagy/Penlight/lua/?.lua;")    
+addpath(               "./luagy/Penlight/lua/?.lua") 
+
+addpath(appdata.."\\folder\\luagy\\?.lua;")
+addpath("/usr/share/folder/luagy/?.lua")
+addpath(                "./luagy/?.lua")
+ 
+addpath(appdata.."\\folder\\?.lua")
+addpath("/usr/share/folder/?.lua")  
 
 Command=require("REPL.command")
 Folder=require "folder"
